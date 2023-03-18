bbe16a4c461b43d64b5ff0f42533f41d81cd759b
Ishwari Kamat <ishwarikmt7@gmail.com>
2023-03-19 01:24:10 +1100

    hosted html using web server

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-const express = require("express");
+var express = require('express')
+var app = express()
 
-const app = express();
-const port = 3030; 
-
-const addTwoNumber = (n1,n2) => {
-    return n1+n2;
-}
-
-console.log(addTwoNumber(10,3));
-app.get("/addTwoNumber", (req,res) => {
-    const n1 = parseInt(req.query.n1);
-    const n2 = parseInt(req.query.n2);
-    const result = addTwoNumber(n1,n2);
-    res.send("The result is", +result);
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + '/index.html')
 })
+
+app.listen(3000)
